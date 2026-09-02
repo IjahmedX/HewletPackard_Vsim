c84ee3d2001fcede3361c9fc589f71718a27e51c
Ijaj Ahmed <mohammed.10.ahmed@kcl.ac.uk>
2026-09-02 22:32:21 +0100

    backlog update

---
+++ rest-service/src/main/java/com/example/restservice/EmployeeController.java
@@ -0,0 +1,20 @@
+package com.example.restservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Creating the REST controller
+@RestController
+@RequestMapping(path = "/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeManager employeeManager;
+
+    @GetMapping(produces = "application/json")
+    public Employees getEmployees() {
+        return employeeManager.getAllEmployees();
+    }
+}
