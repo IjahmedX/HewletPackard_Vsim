aad69fc94e49a2e9e7475ce99e94ded001192928
--unset <mohammed.10.ahmed@kcl.ac.uk>
2026-09-03 00:18:06 +0100

    finalised code for handling post requests

--- rest-service/src/main/java/com/example/restservice/Employee.java
+++ rest-service/src/main/java/com/example/restservice/Employee.java
@@ -2,18 +2,20 @@ package com.example.restservice;
 
 public class Employee {
 
-	private final String employee_id;
-    private final String first_name;
-    private final String last_name;
-    private final String email;
-    private final String title;
+    private String employee_id;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String title;
+
+    public Employee() {
+    }
 
     public Employee(String employee_id,
-                    String first_name,
-                    String last_name,
-                    String email,
-                    String title)
-    {
+            String first_name,
+            String last_name,
+            String email,
+            String title) {
         this.employee_id = employee_id;
         this.first_name = first_name;
         this.last_name = last_name;
@@ -21,41 +23,53 @@ public class Employee {
         this.title = title;
     }
 
-    @Override
-    public String toString()
-    {
-  
-        return "Employee: ID = "
-            + employee_id + ", First Name = "
-            + first_name + ", Last Name = "
-            + last_name + ", email = "
-            + email + ", title = "
-            + title;      
-    }
-
-    public String getId()
-    {
+    public String getId() {
         return employee_id;
     }
 
-    public String getFirstName()
-    {
+    public void setId(String employee_id) {
+        this.employee_id = employee_id;
+    }
+
+    public String getFirstName() {
         return first_name;
     }
 
-    public String getLastName()
-    {
+    public void setFirstName(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLastName() {
         return last_name;
     }
 
-    public String getEmail()
-    {
+    public void setLastName(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
         return email;
     }
 
-    public String getTitle()
-    {
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTitle() {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee: ID = "
+                + employee_id + ", First Name = "
+                + first_name + ", Last Name = "
+                + last_name + ", email = "
+                + email + ", title = "
+                + title;
+    }
 }
